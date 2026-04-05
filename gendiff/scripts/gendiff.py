f5746e79a834cf4b63af270b373bd495dfe30874
Артем Кузнецов <kuznetsov1316@gmail.com>
2026-04-05 15:17:30 +0500

    completing tasks in step 3

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -14,6 +14,7 @@ def main():
     )
 
     args = parser.parse_args()
+    return args.first_file, args.second_file
 
 
 if __name__ == '__main__':
---
+++ gendiff/scripts/read_file.py
@@ -0,0 +1,12 @@
+import json
+
+
+def read_file(path: str) -> dict:
+    with open(path) as file:
+        parsed_file = json.load(file)
+
+    return parsed_file
+
+if __name__ == '__main__':
+    path = str(input('Введите путь до файла: '))
+    read_file(path)
