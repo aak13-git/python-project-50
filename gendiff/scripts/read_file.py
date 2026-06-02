4b944ba9ecdcdc7e5db6917d9ddd9da7e5af7a88
Артем Кузнецов <kuznetsov1316@gmail.com>
2026-06-03 00:12:55 +0700

    fix

--- gendiff/scripts/read_file.py
+++ gendiff/scripts/read_file.py
@@ -7,21 +7,15 @@ import yaml
 def read_file(path: str):
     permission = Path(path).suffix
     if permission == '.json':
-        with open(path) as file:
+        with open(path, encoding='utf-8') as file:
             parsed_file = json.load(file)
         return parsed_file
 
     elif permission in ('.yaml', '.yml'):
-        with open(path) as file:
+        with open(path, encoding='utf-8') as file:
             parsed_file = yaml.safe_load(file)
         return parsed_file
 
     else:
         with open(path, encoding='utf-8') as file:
             return file.read()
-
-
-# if __name__ == '__main__':
-#
-#     path = str(input('Введите путь до файла: '))
-#     read_file(path)
